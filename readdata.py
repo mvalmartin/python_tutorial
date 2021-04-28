997564e4bbab1452f200f2a83e6e1b42865e11ff
Maria Val-Martin <maria.val.martin@gmail.com>
2021-04-28 12:35:47 +0100

    Adding corrected version for readdata module

--- readdata.py
+++ readdata.py
@@ -13,23 +13,23 @@ def read_data(columns, types={}, filename="data/wxobs20170821.txt"):
 
 
     # Initialize my data variable
-    data = {}
-    for column in columns:
-        data[column] = []
+   data = {}
+   for column in columns:
+      data[column] = []
 
      # Read and parse the data file
-     with open(filename, 'r') as datafile:
+   with open(filename, 'r') as datafile:
 
-          # Read the first three lines (header)
-          for _ in range(3):
-             datafile.readline()
+   # Read the first three lines (header)
+        for _ in range(3):
+           datafile.readline()
 
           # Read and parse the rest of the file
-          for line in datafile:
+        for line in datafile:
             split_line = line.split()
-          for column in columns:
-             i = columns[column]
-             t = types.get(column, str)
-             value = t(split_line[i])
-             data[column].append(value)
+            for column in columns:
+               i = columns[column]
+               t = types.get(column, str)
+               value = t(split_line[i])
+               data[column].append(value)
    return data
