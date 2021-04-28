b0087285a25a6205256c00519c0cd088ce6a0315
Maria Val-Martin <maria.val.martin@gmail.com>
2021-04-28 13:02:58 +0100

    Function for computing DPT

--- mysci/computation.py
+++ mysci/computation.py
@@ -1,3 +1,5 @@
+import math
+
 def compute_windchill(t, v):
    """
    Compute the wind chill factor given the temperature and wind speed
@@ -46,3 +48,27 @@ def compute_heatindex(t, rh_pct):
    + (e * t**2) + (f * rh**2) + (g * t**2 * rh)
    + (h * t * rh**2) + (i * t**2 * rh**2)
    return hi
+
+
+def compute_dewpoint(t, rh_pct):
+   """
+   Compute the dew point temperature given the temperature and humidity
+
+   Parameters:
+      t: The temperature in units of F (float)
+      rh_pct: The relative humidity in units of % (float)
+   """
+
+   tempC = (t - 32) * 5 / 9 # Convert temperature from deg F to deg C
+   rh = rh_pct / 100
+
+   b = 18.678
+   c = 257.14 # deg C
+
+   gamma = math.log(rh) + (b * tempC) / (c + tempC)
+   tdp = c * gamma / (b - gamma)
+
+   tdp_F = 9 / 5 * tdp + 32 # Convert deg C to deg F
+   return tdp_F
+
+
