b14d0bcf617551fee9d82e42fe87d4e226777780
Maria Val-Martin <maria.val.martin@gmail.com>
2021-04-28 13:47:22 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="Maria Val-Martin",
+    author_email="maria.val.martin@gmail.com",
+    packages=["mysci"],
+    install_requires=[],
+)
